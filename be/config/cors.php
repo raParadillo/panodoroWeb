490b61fea3e980f89506320339ddcfc5e7cd5eef
RaParadillo <raparadillo06235@liceo.edu.ph>
2026-09-17 17:40:11 +0800

    Allow frontend API requests

--- be/config/cors.php
+++ be/config/cors.php
@@ -20,11 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:5174',
-        'http://127.0.0.1:5174',
-        'https://YOUR-VERCEL-DOMAIN.vercel.app',
+         '*',
     ],
 
     'allowed_origins_patterns' => [],
